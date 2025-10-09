d2a88b7e9316827dc1cb9dbcfee3717764f601b3
Phoebe Smout <144353345+phoebe200506@users.noreply.github.com>
2025-10-09 15:16:00 +0100

    part 3 code
    code to make player move, not working yet

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rigidBody;
+    public float speed = 5.0f;
+    public float jumpForce = 8.0f;
+    public float airControlForce = 10.0f;
+    public float airControlMax = 1.5f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        if ( h != 0.0f)
+        rigidBody.linearVelocity = new Vector2(h * speed, 0.0f);
+    }
+    
+}
