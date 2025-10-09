e4c865de16e3459649c59d31c51a46a0b06127b2
Phoebe Smout <144353345+phoebe200506@users.noreply.github.com>
2025-10-09 16:13:06 +0100

    movement p3 platforms jumping and collisions
    movement finally works, added platforms and jumping and collisons with platforms

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,38 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
-
     Rigidbody2D rigidBody;
     public float speed = 5.0f;
     public float jumpForce = 8.0f;
     public float airControlForce = 10.0f;
     public float airControlMax = 1.5f;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool grounded;
+    // Use this for initialization
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-
     }
-
     // Update is called once per frame
     void Update()
     {
-        
     }
-
     void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
-        if ( h != 0.0f)
+        // check if we are on the ground
+        if (grounded)
         {
-            rigidBody.linearVelocity = new Vector2(h * speed, 0.0f);
+            if (Input.GetAxis("Jump") > 0.0f)
+                rigidBody.AddForce(new Vector2(0.0f, jumpForce), ForceMode2D.Impulse);
+            else
+                rigidBody.linearVelocity = new Vector2(speed * h, rigidBody.linearVelocityY);
         }
-        
     }
-    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            grounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            grounded = false;
+        }
+    }
+
+
 }
