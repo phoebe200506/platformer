9050d77685b0b73cc194ec73f3558f2f8bb376d1
Phoebe Smout <144353345+phoebe200506@users.noreply.github.com>
2025-10-13 11:13:55 +0100

    redoing lesson 1
    but acctually saving the assets

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class PlayerController : MonoBehaviour
 {
+
     Rigidbody2D rigidBody;
-    public float speed = 2.0f;
+    public float speed = 5.0f;
     public float jumpForce = 1.0f;
-    public float airControlForce = 3.0f;
+    public float airControlForce = 5.0f;
     public float airControlMax = 1.5f;
     public bool grounded;
-    // Use this for initialization
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+
     }
+
     // Update is called once per frame
     void Update()
     {
+        
     }
+
     void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
@@ -30,6 +38,7 @@ public class PlayerController : MonoBehaviour
                 rigidBody.linearVelocity = new Vector2(speed * h, rigidBody.linearVelocityY);
         }
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -48,3 +57,7 @@ public class PlayerController : MonoBehaviour
 
      
 }
+ 
+    
+
+
