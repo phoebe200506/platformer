b2b6bd2d372e0792cd12da2fc35513a75a2619d9
Phoebe Smout <144353345+phoebe200506@users.noreply.github.com>
2025-10-20 10:39:36 +0100

    end of week 1 YAYYYYYYY

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
+    public int boxSizeX = 2;
+    public int boxSizeY = 2;
+    
     public Transform attachedPlayer;
     Camera thisCamera;
+    public float blendAmount = 0.05f;
     // Use this for initialization
     void Start()
     {
@@ -13,8 +17,32 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 player = attachedPlayer.transform.position;
-        Vector3 newCamPos = new Vector3(player.x, player.y, transform.position.z);
-        transform.position = newCamPos;
+        Vector3 playerPos = attachedPlayer.transform.position;
+        Vector3 cameraPos = transform.position;
+        float camX, camY;
+        camX = cameraPos.x;
+        camY = cameraPos.y;
+        float screenX0, screenX1, screenY0, screenY1;
+        float box_x0, box_x1, box_y0, box_y1;
+        box_x0 = playerPos.x - boxSizeX;
+        box_x1 = playerPos.x + boxSizeX;
+        box_y0 = playerPos.y - boxSizeY;
+        box_y1 = playerPos.y + boxSizeY;
+        Vector3 bottomLeft = thisCamera.ViewportToWorldPoint(new Vector3(0, 0,
+        0));
+        Vector3 topRight = thisCamera.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        screenX0 = bottomLeft.x;
+        screenX1 = topRight.x;
+        if (box_x0 < screenX0)
+            camX = playerPos.x + 0.5f * (screenX1 - screenX0) - boxSizeX;
+        else if (box_x1 > screenX1)
+            camX = playerPos.x - 0.5f * (screenX1 - screenX0) + boxSizeX;
+        screenY0 = bottomLeft.y;
+        screenY1 = topRight.y;
+        if (box_y0 < screenY0)
+            camY = playerPos.y + 0.5f * (screenY1 - screenY0) - boxSizeY;
+        else if (box_y1 > screenY1)
+            camY = playerPos.y - 0.5f * (screenY1 - screenY0) + boxSizeY;
+        transform.position = new Vector3(camX, camY, cameraPos.z);
     }
 }
